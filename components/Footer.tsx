747e0706ec79ad343c12f43afeb03796bcaf073c
Etan Joseph Heyman <etan@heyman.net>
2024-08-01 09:33:32 -0500

    Background picture fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <footer className='flex flex-col items-center gap-8'>
       <h2 className='text-red-color text-[28px] font-semibold'>מחכה ומתרגשת לפגוש ולהכיר אתכן</h2>
       <div className='flex flex-col items-center'>
-        {/* two wrapping divs just to make the shadow work */}
-        <div className='shadow-photo-inner-shadow rounded-full'>
-          <div className='relative w-[300px] rounded-full -z-10 h-[300px]'>
-            <Image src='/batel.webp' alt='Footer image' unoptimized fill className='aspect-square rounded-full object-[50%_40%] object-cover' />
+        {/* two wrapping divs to make inner shadow and add white background to lighten bg image */}
+        <div className='z-50'>
+          <div className='relative w-[300px] shadow-photo-inner-shadow rounded-full h-[300px]'>
+            <Image src='/batel.webp' alt='Footer image' unoptimized fill className='aspect-square rounded-full -z-10 object-[50%_40%] object-cover' />
           </div>
         </div>
         <Image src='/bateli-logo.webp' alt='Footer logo' unoptimized width={250} height={250} className='' />
