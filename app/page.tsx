1795c287915cb51812e736b3e610419c2503c1bc
Etan Heyman <93338119+EtanHey@users.noreply.github.com>
2024-08-02 07:55:06 -0500

    Update page.tsx
    unoptimize background image

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main dir='rtl' className='w-full gap-3 flex flex-col relative h-fit text-center'>
       <HeroImage />
-      <Image className='-z-50 object-cover object-[30%_50%]' src='/page_background.webp' alt='background' fill priority />
+      <Image unoptimized className='-z-50 object-cover object-[30%_50%]' src='/page_background.webp' alt='background' fill priority />
       <div className='flex flex-col items-center px-6 gap-3'>
         <TimeNdDate />
         <IntroductionSection />
