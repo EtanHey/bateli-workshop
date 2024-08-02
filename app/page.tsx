f2f898c3b26295c6254ca224c179a693344ebda9
Etan Joseph Heyman <etan@heyman.net>
2024-08-02 06:56:09 -0500

    fix white background

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import TimeNdDate from '@/components/TimeNdDate';
 
 export default function Home() {
   return (
-    <main dir='rtl' className='w-full bg-white/50 gap-3 flex flex-col relative h-fit text-center'>
+    <main dir='rtl' className='w-full gap-3 flex flex-col relative h-fit text-center'>
       <HeroImage />
       <Image className='-z-50 object-cover object-[30%_50%]' src='/page_background.webp' alt='background' fill priority />
       <div className='flex flex-col items-center px-6 gap-3'>
