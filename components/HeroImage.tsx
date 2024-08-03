65c43302f440206f93b3885d94b1ee0d2a866562
Etan Joseph Heyman <etan@heyman.net>
2024-08-03 17:26:41 -0500

    Fix buttons, waiting on fixed bg and logo

--- components/HeroImage.tsx
+++ components/HeroImage.tsx
@@ -6,7 +6,7 @@ import SignupButton from './SignupButton';
 const myFont = localFont({src: '../fonts/hachalutzim.otf'});
 const HeroImage = () => {
   return (
-    <div className='relative w-full z-50 xl:h-[76svh] h-[344px]'>
+    <div className='relative w-full xl:h-[76svh] h-[344px]'>
       <Image src='/hero_image.webp' alt='Hero image' unoptimized className='object-cover max-md:object-top' fill priority />
       <div className='absolute items-center flex flex-col gap-1 w-full text-center top-[5%] xl:top-[8%]'>
         <div className='text-4xl flex flex-col gap-[8.3rem] xl:gap-96'>
