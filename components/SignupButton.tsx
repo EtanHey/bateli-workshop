10978f967aa519b0cfb2a1a832850a521c60936a
Etan Heyman <93338119+EtanHey@users.noreply.github.com>
2024-08-03 16:05:27 -0500

    Update SignupButton.tsx
    Z-50

--- components/SignupButton.tsx
+++ components/SignupButton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const SignupButton = ({className}: {className?: string}) => {
   return (
-    <Link href='https://meshulam.co.il/s/2bb7dbbf-266c-f513-53c4-950b3f6f5a4f' className={`text-2xl  px-7 rounded-full w-fit ${className ? className : ''}`}>
+    <Link href='https://meshulam.co.il/s/2bb7dbbf-266c-f513-53c4-950b3f6f5a4f' className={`text-2xl z-50 px-7 rounded-full w-fit ${className ? className : ''}`}>
       להרשמה
     </Link>
   );
