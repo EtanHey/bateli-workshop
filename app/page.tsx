375d289cc3305703c6e5cc787cb545cb3f996446
Etan Joseph Heyman <etan@heyman.net>
2024-08-03 22:19:33 -0500

    Fix background, hopefully

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,18 @@ import TimeNdDate from '@/components/TimeNdDate';
 
 export default function Home() {
   return (
-    <main dir='rtl' className='w-full  gap-3 flex flex-col relative h-fit text-center'>
+    <main dir='rtl' className='w-full flex flex-col h-fit text-center'>
       <HeroImage />
-      {/* <Image unoptimized className='-z-50 object-contain object-top' src='/page_background.webp' alt='background' fill priority /> */}
-      <Image unoptimized className='-z-50 object-top object-contain' src='/page_background.webp' alt='background' fill priority />
-      <div className='flex flex-col items-center px-6 gap-3'>
-        <TimeNdDate />
-        <IntroductionSection />
-        <DescriptionMultiSection />
-        <SignupButton className='bg-red-color shadow-red-signup-button-inner' />
-        <WorkshopInfoSection />
-        <Footer />
+      <div className='w-full pt-3 gap-3 bg-white/30 flex flex-col relative h-fit text-center'>
+        <Image unoptimized className='-z-50 object-top object-cover' src='/page_background.webp' alt='background' fill priority />
+        <div className='flex flex-col items-center px-6 gap-3'>
+          <TimeNdDate />
+          <IntroductionSection />
+          <DescriptionMultiSection />
+          <SignupButton className='bg-red-color shadow-red-signup-button-inner' />
+          <WorkshopInfoSection />
+          <Footer />
+        </div>
       </div>
     </main>
   );
