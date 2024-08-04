d267527e1956e42928514356ba4e9110cfa3f6be
Etan Heyman <93338119+EtanHey@users.noreply.github.com>
2024-08-04 10:12:33 -0500

    Update HeroImage.tsx
    Optimize

--- components/HeroImage.tsx
+++ components/HeroImage.tsx
@@ -7,7 +7,7 @@ const myFont = localFont({src: '../fonts/hachalutzim.otf'});
 const HeroImage = () => {
   return (
     <div className='relative w-full xl:h-[76svh] h-[344px]'>
-      <Image src='/hero_image.webp' alt='Hero image' unoptimized className='object-cover max-md:object-top' fill priority />
+      <Image src='/hero_image.webp' alt='Hero image' className='object-cover max-md:object-top' fill priority />
       <div className='absolute items-center flex flex-col gap-1 w-full text-center top-[5%] xl:top-[8%]'>
         <div className='text-4xl text-white flex flex-col gap-[8.3rem] xl:gap-96'>
           <h1 className={`font-semibold ${myFont.className}`}>הגיע הזמן ״ לאהוב אותך ״</h1>
